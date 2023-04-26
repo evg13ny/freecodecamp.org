37db831f5341ea7723db40dfc0cfb69023f27c4a
Evgeny Polyakov <evg_pol@yahoo.com>
2023-04-26 23:04:03 +0300

    added tasks 02.01.012 - 02.01.093

---
+++ Section 2. JavaScript Algorithms and Data Structures/Basic JavaScript/task02.01.040.Nest one Array within Another Array.js
@@ -0,0 +1,2 @@
+// Only change code below this line
+const myArray = [["bread", 1], ["sausage", 2]];
---
+++ Section 2. JavaScript Algorithms and Data Structures/Basic JavaScript/task02.01.048.Shopping List.js
@@ -0,0 +1,8 @@
+const myList = [
+    ["eggs", 2],
+    ["butter", 10],
+    ["bread", 2],
+    ["coffe", 1],
+    ["milk", 20],
+    ["pie", 30]
+];
